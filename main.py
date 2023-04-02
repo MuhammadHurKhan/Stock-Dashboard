4003a86f83f061ec9c984377d635fd3c8ceeaf59
Muhammad Hur Khan <122796904+MuhammadHurKhan@users.noreply.github.com>
2023-04-02 15:12:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ end_date=st.sidebar.date_input("Select ending date:")
 
 #Retrieving the Data
 try:
-    data = yf.download(ticker+'.NAS', start=start_date, end=end_date)
+    data = yf.download(ticker, start=start_date, end=end_date)
     #Plotting Data
     fig = px.line(data, x=data.index, y=data['Adj Close'], title = ticker)
     st.plotly_chart(fig)
